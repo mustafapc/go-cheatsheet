fe71c87d4f1e3a82c13db22057cb05356157e148
unknown <mypcdk1@gmail.com>
2025-03-02 16:53:47 +0100

    edit

--- hi.go
+++ hi.go
@@ -12,6 +12,13 @@ func add(x int, y int) (int, int) {
 return x + y, 3
 }
 func main(){
-	a, b := add(5, 8);
-	fmt.Println("hello world", rand.Intn(15), "pi is equal to:", math.Pi, "5 + 8", a, "adsf", b)
+	/*types are int int8 int16 int32 int64
+	uint uint8 uint16 uint32 uint64
+	byte = uint8
+	*/ 
+	a, b := add(5, 8)
+	var c int
+	var d int
+	c, d = add(3, 8)
+	fmt.Println("hello world", rand.Intn(15), "pi is equal to:", math.Pi, "5 + 8", a, "adsf", b, c, d)
 }
